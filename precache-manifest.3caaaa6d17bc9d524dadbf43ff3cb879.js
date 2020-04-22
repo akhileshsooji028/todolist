04228449b5d322018dc5085f05a8cd929e6e3d18
Akhilesh Sooji <akhisooji028@gmail.com>
2020-04-22 12:41:17 +0530

    Updates

---
+++ precache-manifest.3caaaa6d17bc9d524dadbf43ff3cb879.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6df60b5d762c16cb4ca978c753d354",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "cda22366e40db4864929",
+    "url": "/todolist/static/css/main.67a1b307.chunk.css"
+  },
+  {
+    "revision": "7e5587bb5498c9980d22",
+    "url": "/todolist/static/js/2.f1049855.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.f1049855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cda22366e40db4864929",
+    "url": "/todolist/static/js/main.ecce4f94.chunk.js"
+  },
+  {
+    "revision": "5dc327340e8e32fc3756",
+    "url": "/todolist/static/js/runtime-main.c337c584.js"
+  }
+]);
